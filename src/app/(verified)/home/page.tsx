3adee3a1ce379c8295c4205fee26070363b50503
Aaditya Mahanta <aadityamahanta07@gmail.com>
2024-03-17 15:39:31 +0530

    ds

--- src/app/(verified)/home/page.tsx
+++ src/app/(verified)/home/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Page() {
-  return <main className="ml-[70px]"></main>;
+  return <main className="ml-[70px]">
+    Metrics go here
+  </main>;
 }
